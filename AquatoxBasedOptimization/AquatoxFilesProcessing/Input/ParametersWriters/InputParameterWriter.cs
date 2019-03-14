5c1c26144f64ab7ad71fa49c49fef78b1f8bd67d
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-03-14 22:24:43 +0200

    Add parameter substitution

---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Input/ParametersWriters/InputParameterWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Input.ParametersWriters
+{
+    public class InputParameterWriter : IInputParameterWriter
+    {
+        private string _inputString;
+        private string _leftImmutablePart;
+        private string _rightImmutablePart;
+
+        private StringBuilder stringBuilder;
+
+        public string ParameterName { get; }
+        public ParameterLocationType ParameterLocationType { get; private set; }
+
+        public InputParameterWriter(string inputString, string parameter)
+        {
+            ParameterName = parameter;
+
+            IdentifyImmutableParts(inputString);
+        }
+
+        public string Write(string parameterValue)
+        {
+            stringBuilder.Append(parameterValue);
+            stringBuilder.Append(_rightImmutablePart);
+
+            return stringBuilder.ToString();
+        }
+
+        private void IdentifyImmutableParts(string inputString)
+        {
+            // Initialize input string
+            _inputString = inputString;
+            // Split it with the parameter name
+            var splitted = inputString.Split(new string[] { ParameterName }, StringSplitOptions.None);
+            // Get its left and right parts
+            _leftImmutablePart = splitted[0];
+            _rightImmutablePart = splitted[1];
+            // Identify the type
+            if (_leftImmutablePart == "")
+            {
+                ParameterLocationType = ParameterLocationType.Left;
+            }
+            else if (_rightImmutablePart == "")
+            {
+                ParameterLocationType = ParameterLocationType.Right;
+            }
+            else
+            {
+                if (_leftImmutablePart == "" && _rightImmutablePart == "")
+                    throw new Exception("Wrong parameter location in parameter string");
+
+                ParameterLocationType = ParameterLocationType.Center;
+            }
+
+            // Set the initial value for the string builder
+            stringBuilder = new StringBuilder(_leftImmutablePart);
+        }
+    }
+}
---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Input/ParametersWriters/ParameterLocationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Input.ParametersWriters
+{
+    public enum ParameterLocationType
+    {
+        Left,
+        Center,
+        Right
+    }
+}
