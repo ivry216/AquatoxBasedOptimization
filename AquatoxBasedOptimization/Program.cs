537ba9e06931a28378ea75630b53f1f2f584ea0d
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-03-22 06:45:16 +0200

    Add current test code

--- AquatoxBasedOptimization/Program.cs
+++ AquatoxBasedOptimization/Program.cs
@@ -1,7 +1,11 @@
-﻿using AquatoxBasedOptimization.ExternalProgramOperating;
+﻿using AquatoxBasedOptimization.AquatoxFilesProcessing.Input;
+using AquatoxBasedOptimization.AquatoxFilesProcessing.Input.ParametersWriters;
+using AquatoxBasedOptimization.ExternalProgramOperating;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +16,47 @@ namespace AquatoxBasedOptimization
     {
         static void Main(string[] args)
         {
-            SimpleSingleLauncher simpleSingleLauncher = new SimpleSingleLauncher();
-            simpleSingleLauncher.File = new System.IO.FileInfo(@"C:/Users/ivanry/AQUATOX R3.2/PROGRAM/aquatox.exe");
-            simpleSingleLauncher.SetParameters("ECSAVE \"C:/Users/ivanry/AQUATOX R3.2/STUDIES/Lake Pyhajarvi Finland.txt\" \"test1.txt\"");
+            //SimpleSingleLauncher simpleSingleLauncher = new SimpleSingleLauncher();
+            //simpleSingleLauncher.File = new FileInfo(@"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/PROGRAM/aquatox.exe");
+            //simpleSingleLauncher.SetParameters("EPSAVE \"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/STUDIES/Lake Pyhajarvi Finland.txt\" \"test.txt\"");
 
             Console.WriteLine("Starting...");
 
-            simpleSingleLauncher.Run();
+            //simpleSingleLauncher.Run();
+
+            string inputFileTemp = @"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/STUDIES/Lake Pyhajarvi Finland Template.txt";
+            List<string> parameters = new List<string> { "_param1_" };
+            AquatoxInputFileProcessor inputFileProcessor = new AquatoxInputFileProcessor(inputFileTemp, parameters);
+
+            //string fileToSave = @"C:/Users/ivanry/Documents/Repositories/AquatoxBasedOptimization/AquatoxBasedOptimization/AquatoxBasedOptimization/bin/Debug/test_out.txt";
+            //var dict = parameters.ToDictionary(item => item, item => "1");
+            //inputFileProcessor.SetParametersBySubstitution(fileToSave, dict);
+
+            ConcurrentBag<string[]> bag = new ConcurrentBag<string[]>();
+
+            Parallel.For(1, 10, (i) =>
+            {
+                string fileToSave = @"C:/Users/ivanry/Documents/Repositories/AquatoxBasedOptimization/AquatoxBasedOptimization/AquatoxBasedOptimization/bin/Debug/test_out_" + i + ".txt";
+                var dict = parameters.ToDictionary(item => item, item => 0.ToString());
+                inputFileProcessor.SetParametersBySubstitution(fileToSave, dict);
+                SimpleSingleLauncher simpleSingleLauncher = new SimpleSingleLauncher();
+                simpleSingleLauncher.File = new FileInfo(@"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/PROGRAM/aquatox.exe");
+
+                string resultiveFileName = "test" + i + ".txt";
+                simpleSingleLauncher.SetParameters("EPSAVE " + fileToSave + " \"" + resultiveFileName + "\"");
+                simpleSingleLauncher.Run();
+
+                bag.Add(File.ReadAllLines(resultiveFileName));
+            });
+
+            string[] strings = File.ReadAllLines("output.txt");
+            string[] stringsInput = File.ReadAllLines(@"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/STUDIES/Lake Pyhajarvi Finland.txt");
+            int counterStart = strings.Skip(1).Where(str => str.Contains("{")).Count();
+            int counterEnd = strings.Skip(1).Where(str => str.Contains("}")).Count();
+
+            int counterStartInput = stringsInput.Where(str => str.Contains("{")).Count();
+            int counterEndInput = stringsInput.Where(str => str.Contains("}")).Count();
+
 
             Console.WriteLine("End!");
             Console.Read();
