6e206af1290f0403f08ee4aa541a26e84dfbea91
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-05-24 17:10:12 +0300

    Add time series object

---
+++ AquatoxBasedOptimization/Data/ITimeSeries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Data
+{
+    public interface ITimeSeries
+    {
+        int Size { get; }
+        double[] Values { get; }
+        DateTime[] Times { get; }
+        string Name { get; }
+    }
+}
---
+++ AquatoxBasedOptimization/Data/TimeSeries.cs
@@ -0,0 +1,49 @@
+﻿using AquatoxBasedOptimization.Extensions.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Data
+{
+    public class TimeSeries : ITimeSeries
+    {
+        #region Properties
+
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public double[] Values { get; set; }
+
+        public DateTime[] Times { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+
+        public TimeSeries(string name, double[] values, DateTime[] times)
+        {
+            // Set name
+            Name = name;
+
+            // Initialize size
+            Size = values.Length;
+
+            // Check the size
+            if (Size != times.Length)
+                throw new Exception("The size of timestamps and values are different");
+
+            // Initialize arrays
+            Values = new double[Size];
+            Times = new DateTime[Size];
+
+            // Assign values
+            Values.CopyFrom(values);
+            Times.CopyFrom(times);
+        }
+
+        #endregion Constructor
+    }
+}
