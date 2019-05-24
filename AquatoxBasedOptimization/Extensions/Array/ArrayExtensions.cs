ce3839228f76418685098c89b7270586e1b55548
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-05-24 17:10:27 +0300

    Add array extension

---
+++ AquatoxBasedOptimization/Extensions/Array/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Extensions.Array
+{
+    static class ArrayExtensions
+    {
+        public static void CopyFrom<T>(this T[] current, T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                current[i] = array[i];
+            }
+        }
+    }
+}
