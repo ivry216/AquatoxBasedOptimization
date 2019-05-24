8ee73dad2bef097b31a041fbefc493649fe700b7
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-05-24 17:25:42 +0300

    Add class for getting the right time series lines

---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/DayObservationsGetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Output.Converter
+{
+    public class DayObservationsGetter : IDayObservationsGetter
+    {
+        #region Fields
+
+        private const string _label = "\"ObjID\": 1010";
+
+        #endregion Fields
+
+        #region Getter
+
+        public List<string> GetLinesOfData(List<string> allLines)
+        {
+            // Look for indices of lines, which contain labels
+            var linesContainingLabel = allLines
+                .Select((line, index) => (line, index))
+                .Where(pair => pair.line.Contains(_label))
+                .Select(pair => pair.index)
+                .ToList();
+            // Increment indices
+            linesContainingLabel = linesContainingLabel.Select(index => index + 1).ToList();
+
+            // Select the right lines
+            var linesWithObservations = linesContainingLabel
+                .Select(index => allLines[index])
+                .ToList();
+
+            return linesWithObservations;
+        }
+
+        #endregion Getter
+    }
+}
