5cd4904d281fa954e632c90692e5277fdb2a783c
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-05-26 20:18:34 +0300

    Add another processors

---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/ILineInformationExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Output.Converter
+{
+    public interface ILineInformationExtractor
+    {
+        (DateTime Date, Dictionary<string, double>) ExtractData(string line);
+    }
+}
---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/LineInformationExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Output.Converter
+{
+    public class LineInformationExtractor : ILineInformationExtractor
+    {
+        public (DateTime Date, Dictionary<string, double>) ExtractData(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
