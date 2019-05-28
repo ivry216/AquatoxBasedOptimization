78df44d2b8de719010eb31941c0c9c83c4fcac65
Ivan <ivan.s.ryzhikov@gmail.com>
2019-05-28 07:48:18 +0300

    Add empty classes for time series converting

---
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/ITimeSeriesBuilder.cs
@@ -0,0 +1,14 @@
+﻿using AquatoxBasedOptimization.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Output.Converter
+{
+    public interface ITimeSeriesBuilder
+    {
+        ITimeSeries Build(List<(DateTime Date, Dictionary<string, double> Variables)> parsedData);
+    }
+}
