60be2b8de3ced66ee2b0cc19e9efb001c5d77578
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-06-02 18:58:10 +0300

    Improve reading output variables from excel

--- AquatoxBasedOptimization/Data/OutputVariables/OutputVariablesReaderFromExcel.cs
+++ AquatoxBasedOptimization/Data/OutputVariables/OutputVariablesReaderFromExcel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AquatoxBasedOptimization.Data.OutputVariables
 {
-    class OutputVariablesReaderFromExcel
+    public class OutputVariablesReaderFromExcel : IOutputVariablesReader
     {
+        private const string _fileName = "OutputVariables.xlsx";
+
+        public Dictionary<string, int> Read()
+        {
+            // Initialize variables-indices pairs
+            Dictionary<string, int> variablesIndices = new Dictionary<string, int>();
+
+            FileInfo file = new FileInfo(_fileName);
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                //get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                //
+                int nRows = worksheet.Dimension.End.Row;
+                //
+                for (int row = 2; row <= nRows; row++)
+                {
+                    variablesIndices.Add((string)worksheet.Cells[row, 1].Value, (int)worksheet.Cells[row, 2].Value);
+                }
+            }
+
+            return variablesIndices;
+        }
     }
 }
