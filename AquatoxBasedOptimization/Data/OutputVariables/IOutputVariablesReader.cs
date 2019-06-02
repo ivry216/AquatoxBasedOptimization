a9d37e4a05a7f9be884a203b7147a9a64d7359df
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-06-02 18:49:30 +0300

    Add output variables reader from excel

---
+++ AquatoxBasedOptimization/Data/OutputVariables/IOutputVariablesReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Data.OutputVariables
+{
+    public interface IOutputVariablesReader
+    {
+        Dictionary<string, int> Read();
+    }
+}
