6e6f989f9f6428dd5de8ef8062ef2190a9ddaa28
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-06-04 21:50:33 +0300

    Add output observations

---
+++ AquatoxBasedOptimization/Data/IOutputObservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Data
+{
+    public interface IOutputObservation
+    {
+        string Name { get; }
+        Dictionary<double, ITimeSeries> DepthRelatedObservations { get; }
+    }
+}
---
+++ AquatoxBasedOptimization/Data/OutputObservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquatoxBasedOptimization.Data
+{
+    public class OutputObservation : IOutputObservation
+    {
+        public string Name { get; private set; }
+
+        public Dictionary<double, ITimeSeries> DepthRelatedObservations { get; private set; }
+
+        public OutputObservation(string name, Dictionary<double, ITimeSeries> depthRelatedObservations)
+        {
+            Name = name;
+            DepthRelatedObservations = depthRelatedObservations;
+        }
+    }
+}
