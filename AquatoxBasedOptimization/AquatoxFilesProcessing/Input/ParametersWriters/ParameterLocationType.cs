e9f363550c054fc766c784a18d44af4d3a86b745
Ivan <ivan.s.ryzhikov@gmail.com>
2019-07-20 20:42:18 +0300

    Delete usings

--- AquatoxBasedOptimization/AquatoxFilesProcessing/Input/ParametersWriters/ParameterLocationType.cs
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Input/ParametersWriters/ParameterLocationType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Input.ParametersWriters
+﻿namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Input.ParametersWriters
 {
     public enum ParameterLocationType
     {
--- AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/ITimeSeriesBuilder.cs
+++ AquatoxBasedOptimization/AquatoxFilesProcessing/Output/Converter/ITimeSeriesBuilder.cs
@@ -1,9 +1,6 @@
 ﻿using AquatoxBasedOptimization.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AquatoxBasedOptimization.AquatoxFilesProcessing.Output.Converter
 {
