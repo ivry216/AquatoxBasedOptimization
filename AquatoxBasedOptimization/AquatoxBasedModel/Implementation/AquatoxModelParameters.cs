4eddbcfa0d172ba057a2df39bbf052aac18a80ff
Ivan <ivan.s.ryzhikov@gmail.com>
2019-08-06 17:52:36 +0300

    Style aquatox model parameters

--- AquatoxBasedOptimization/AquatoxBasedModel/Implementation/AquatoxModelParameters.cs
+++ AquatoxBasedOptimization/AquatoxBasedModel/Implementation/AquatoxModelParameters.cs
@@ -9,10 +9,10 @@ namespace AquatoxBasedOptimization.AquatoxBasedModel.Implementation
     {
         private Dictionary<string, string> _inputParameters;
 
-        public string InputFilePath { get; } = @"C:/Users/ivanry/Documents/Repositories/AquatoxBasedOptimization/AquatoxBasedOptimization/JupyterNotebooks/Lake Pyhajarvi Finland.txt";
+        //string inputFileTemp = @"C:/Users/ivanry/Documents/Repositories/AquatoxBasedOptimization/AquatoxBasedOptimization/JupyterNotebooks/Lake Pyhajarvi Finland.txt";
+        public string InputFilePath { get; } = @"C:/Users/Ivan/Repositiries/AquatoxBasedOptimization/JupyterNotebooks/Lake Pyhajarvi Finland.txt";
         public string AquatoxExecutablePath { get; } = @"C:/Users/ivanry/fixed_aquatox/AQUATOX R3.2/PROGRAM/aquatox.exe";
         public string CurrentDirectory { get; } = Directory.GetCurrentDirectory();
-
         public string NumericRepresentationFormat { get; } = "0.00000000000000E+0000";
 
         public Dictionary<string, string> InputParameters
