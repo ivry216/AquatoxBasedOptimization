814243ce316eed14c93353eb85710d879166b038
Ivan <ivan.s.ryzhikov@gmail.com>
2019-08-19 21:18:38 +0300

    Add class for variable description

---
+++ AquatoxBasedOptimization/AquatoxBasedProblem/Implementation/AquatoxParameterToTune.cs
@@ -0,0 +1,22 @@
+﻿namespace AquatoxBasedOptimization.AquatoxBasedProblem.Implementation
+{
+    public class AquatoxParameterToTune
+    {
+        public string Name { get; }
+        public double InitialValue { get; }
+        public double? SoftMaxConstrain { get; }
+        public double? SoftMinConstrain { get; }
+        public double? HardMaxConstrain { get; }
+        public double? HardMinConstrain { get; }
+
+        public AquatoxParameterToTune(string name, double initialValue, double? hardMax = null, double? hardMin = null, double? softMax = null, double? softMin = null)
+        {
+            Name = name;
+            InitialValue = initialValue;
+            HardMaxConstrain = hardMax;
+            HardMinConstrain = hardMin;
+            SoftMinConstrain = softMax;
+            SoftMaxConstrain = softMax;
+        }
+    }
+}
