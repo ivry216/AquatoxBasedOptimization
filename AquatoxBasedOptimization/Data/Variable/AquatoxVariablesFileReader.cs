0f564b0521fc39bc490567c7b2c6cfc248bec198
Ivan <ivan.s.ryzhikov@gmail.com>
2019-08-19 21:41:05 +0300

    Relocate variables reader

---
+++ AquatoxBasedOptimization/Data/Variable/AquatoxVariablesFileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AquatoxBasedOptimization.AquatoxBasedProblem.Implementation;
+using OfficeOpenXml;
+
+namespace AquatoxBasedOptimization.Data.Variable
+{
+    public class AquatoxVariablesFileReader
+    {
+        public Dictionary<string, AquatoxParameterToTune> ReadParameters(string fileName)
+        {
+            var parameters = new Dictionary<string, AquatoxParameterToTune>();
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(fileName)))
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
